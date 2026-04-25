fe70848a7dcf9d15a5f64a33e9d52c4547f4665d
Baebin <baebine@naver.com>
2026-04-25 16:27:49 +0900

    Initial commit

---
+++ src/main/java/com/piebin/bingweb/BingWebApplication.java
@@ -0,0 +1,13 @@
+package com.piebin.bingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BingWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/piebin/bingweb/BingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piebin.bingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
