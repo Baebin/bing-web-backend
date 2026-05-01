ce93827f1d2fa412d7d0a60d4f1dfbba3cdb07c2
Baebin <baebine@naver.com>
2026-05-01 23:33:42 +0900

    fix: add missing @Repository annotation

--- src/main/java/com/piebin/bingweb/features/file/repository/FileInfoRepository.java
+++ src/main/java/com/piebin/bingweb/features/file/repository/FileInfoRepository.java
@@ -2,9 +2,11 @@ package com.piebin.bingweb.features.file.repository;
 
 import com.piebin.bingweb.features.file.domain.FileInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface FileInfoRepository extends JpaRepository<FileInfo, Long> {
     Optional<FileInfo> findByPathAndName(String path, String originName);
 }
